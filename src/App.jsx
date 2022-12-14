c7ed671a28c14b3fb38f9d8995c971dfc07ccd9a
Juan Jose Ruiz Sanchez <juanjoserusa@gmail.com>
2022-12-14 09:16:08 +0100

    Creacion proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <div className="App">
+    <main>
+    TikTok clone
+    </main>
+      
+    </div>
+  );
+}
+
+export default App;
